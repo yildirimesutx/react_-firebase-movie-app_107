33237c97187d418a499f0540001871c65df41c38
mesut <yildirimesutcw@gmail.com>
2022-04-12 00:13:39 +0300

    v2

---
+++ src/auth/firebase.js
@@ -0,0 +1,50 @@
+import { initializeApp } from "firebase/app";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+
+//* https://firebase.google.com/docs/auth/web/start
+//* https://console.firebase.google.com/ => project settings
+const firebaseConfig = {
+    apiKey: "AIzaSyDZ5NyX6_jVs74Ms1H60ZhTyowAX-UvTlw",
+    authDomain: "movie-app-53e15.firebaseapp.com",
+    projectId: "movie-app-53e15",
+    storageBucket: "movie-app-53e15.appspot.com",
+    messagingSenderId: "944797644565",
+    appId: "1:944797644565:web:dfc5a36455ea49a7391b1c"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+export const createUser = async (email, password, navigate) => {
+  try {
+    let userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    navigate("/");
+    console.log(userCredential);
+  } catch (err) {
+    alert(err.message);
+  }
+};
+
+export const signIn = async (email, password, navigate) => {
+  try {
+    let userCredential = await signInWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    navigate("/");
+    console.log(userCredential);
+  } catch (err) {
+    alert(err.message);
+  }
+};
